632e6989539da0fac818ebbd5028662a68f95df0
Santhos Baala RS <rssanthosbaala@gmail.com>
2013-09-04 17:00:49 +0530

    Added a convenience header, that imports all the headers in the library.

---
+++ PGN Reader/PGNReader.h
@@ -0,0 +1,28 @@
+//
+//  PGNReader.h
+//  PGN Reader
+//
+//  Created by Santhosbaala RS on 30/08/13.
+//  Copyright (c) 2013 64cloud. All rights reserved.
+//
+
+#ifndef PGN_Reader_PGNReader_h
+#define PGN_Reader_PGNReader_h
+
+#ifdef __cplusplus
+extern "C++" {
+#endif
+
+#import "PGNException.h"
+#import "PGNFile.h"
+#import "PGNGame.h"
+#import "PGNTokenizer.h"
+#import "PGNVariationFactory.h"
+#import "PGNVariation.h"
+#import "PGNMove.h"
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif
